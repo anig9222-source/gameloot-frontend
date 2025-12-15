3567823c59b1381b3362e4831a5550d35c7948a9
GameLoot Deployment <deployment@gameloot.com>
2025-12-15 19:10:18 +0000

    Integrate MonetizeTag ad scripts (OnClick + Push Notifications)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,6 +22,12 @@ export default function RootLayout() {
     <>
       <Head>
         <meta name="monetag" content="84a69637dddbaf50918ff9457037a31a" />
+        {/* MonetizeTag OnClick Ad (Pungent tag - Zone: 10330829) */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(s){s.dataset.zone='10330829',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`
+        }} />
+        {/* MonetizeTag Push Notifications (Pleasant tag - Zone: 10330455) */}
+        <script src="https://3nbf4.com/act/files/tag.min.js?z=10330455" data-cfasync="false" async />
       </Head>
       <LanguageProvider>
         <AuthProvider>
